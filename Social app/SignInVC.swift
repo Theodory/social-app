d7e977f8cf05523dac9f97ba285f5f51c1e52ed1
Twalipo <echasste@gmail.com>
2017-07-06 12:55:57 +0300

    facebook auhtentication completed

--- Social app/SignInVC.swift
+++ Social app/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FacebookCore
+import FacebookLogin
 
 class SignInVC: UIViewController {
 
@@ -15,5 +18,31 @@ class SignInVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func facebookBtnPressed(_ sender: Any) {
+        let facebookLogin = LoginManager()
+        facebookLogin.logIn([.email], viewController: self) { (loginResult) in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User canceled login")
+            case .success( _ , _ , let token):
+                print("Logged in successfully")
+                let credential = FacebookAuthProvider.credential(withAccessToken: token.authenticationToken)
+                
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+            Auth.auth().signIn(with: credential) { (user, error) in
+                if error != nil {
+                    print("SIGNIN: Unable to Authenticate with firebase - \(error.debugDescription)")
+                } else {
+                    print("SIGNIN: successfully authenticated with firebase")
+                }
+        }
+    }
 }
 
